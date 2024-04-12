1ae9054ed8b2f9ece8f060d364fd143b55799fa4
William Kang <wkang360@gmail.com>
2024-04-12 12:41:49 -0400

    Update DoorbellBomb.ino

--- DoorbellBomb.ino
+++ DoorbellBomb.ino
@@ -1,13 +1,15 @@
 #include <LiquidCrystal_I2C.h>
 
-char[] options = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()-";
-char[] typedIn = [];
+// unplug D12 to upload code :(
+
+String options = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()-";
+String typedIn = "";
 int currentColumn = 0;
 
 int yellowButton = 32;  // goes low when pushed
 int redButton = 33;     // goes high when toggled
-int upPin = 35;         // goes high when pushed
-int downPin = 34;       // goes high when pushed
+int upPin = 2;          // goes high when pushed
+int downPin = 4;        // goes high when pushed
 int leftPin = 25;       // goes to low when pushed
 int rightPin = 26;      // goes low when pushed
 
@@ -18,20 +20,65 @@ void setup() {
   lcd.init();
   // turn on LCD backlight
   lcd.backlight();
+  pinMode(yellowButton, INPUT);
+  pinMode(redButton, INPUT);
   pinMode(leftPin, INPUT);
   pinMode(rightPin, INPUT);
   pinMode(upPin, INPUT);
   pinMode(downPin, INPUT);
+  lcd.setCursor(0, 0);
+  lcd.print("Password:");
 }
 
 void loop() {
-  // set cursor to first column, first row
   lcd.setCursor(0, 0);
-  // print message
-  lcd.print(analogRead(leftPin) < 100 ? "left on" : "left off");
-  // set cursor to first column, second row
+
+  // up and down cycles through the options, left and right cycles through the typed in characters
+  // if (digitalRead(upPin) == HIGH) {
+  //   currentColumn = (currentColumn + 1) % options.length();
+  //   lcd.setCursor(0, 1);
+  //   lcd.print(options.substring(currentColumn, currentColumn + 16));
+  //   delay(200);
+  // } else if (digitalRead(downPin) == HIGH) {
+  //   currentColumn = (currentColumn - 1 + options.length()) % options.length();
+  //   lcd.setCursor(0, 1);
+  //   lcd.print(options.substring(currentColumn, currentColumn + 16));
+  //   delay(200);
+  // } else if (digitalRead(leftPin) == LOW) {
+  //   typedIn += options[currentColumn];
+  //   lcd.setCursor(0, 0);
+  //   lcd.print("Password: " + typedIn);
+  //   delay(200);
+  // } else if (digitalRead(rightPin) == LOW) {
+  //   typedIn = typedIn.substring(0, typedIn.length() - 1);
+  //   lcd.setCursor(0, 0);
+  //   lcd.print("Password: " + typedIn);
+  //   delay(200);
+  // }
+
+  // show the status of each button
+  lcd.print(analogRead(redButton) > 130 ? 1 : 0);
+  lcd.print(analogRead(yellowButton) < 250 ? 1 : 0);
+  lcd.print(analogRead(leftPin) < 300 ? 1 : 0);
+  lcd.print(analogRead(rightPin) < 400 ? 1 : 0);
+  lcd.print(analogRead(upPin) > 0 ? 1 : 0);
+  lcd.print(analogRead(downPin) < 250 ? 1 : 0);
+
   lcd.setCursor(0, 1);
-  lcd.print(analogRead(rightPin) < 100 ? "right on" : "right off");
+
+  // calibration
+  // lcd.print(analogRead(redButton));
+  // lcd.print(" ");
+  // lcd.print(analogRead(yellowButton));
+  // lcd.print(" ");
+  // lcd.print(analogRead(upPin));
+  // lcd.print("  ");
+  // lcd.print(analogRead(downPin));
+  // lcd.setCursor(0, 1);
+  // lcd.print(analogRead(leftPin));
+  // lcd.print("  ");
+  // lcd.print(analogRead(rightPin));
+
   delay(100);
   lcd.clear();
 }
