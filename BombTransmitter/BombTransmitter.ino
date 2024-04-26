5d42cef715f0417f8f609429828fd91f370a88b0
William Kang <william@wkang.ca>
2024-04-26 12:20:44 -0400

    Update BombTransmitter.ino

--- BombTransmitter/BombTransmitter.ino
+++ BombTransmitter/BombTransmitter.ino
@@ -6,29 +6,29 @@ String options = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghij
 String typedIn = "";
 int currentColumn = 0;
 
-int redButton = 33;     // goes high when toggled
-int yellowButton = 32;  // goes low when pushed
-int upPin = 2;          // goes high when pushed
-int downPin = 4;        // goes high when pushed
-int leftPin = 25;       // goes to low when pushed
-int rightPin = 26;      // goes low when pushed
+const int redButton = 33;     // goes high when toggled
+const int yellowButton = 32;  // goes low when pushed
+const int upPin = 2;          // goes high when pushed
+const int downPin = 4;        // goes high when pushed
+const int leftPin = 25;       // goes to low when pushed
+const int rightPin = 26;      // goes low when pushed
 
-int buttons[] = {redButton, yellowButton, upPin, downPin, leftPin, rightPin};
+int buttons[] = { redButton, yellowButton, upPin, downPin, leftPin, rightPin };
 
-// int buttonState[NUMBUTTONS];
-// int lastButtonState[NUMBUTTONS];
-// boolean buttonIsPressed[NUMBUTTONS];
+// int buttonState[] = { 0, 0, 0, 0, 0, 0 };
+// int lastButtonState[] = { 0, 0, 0, 0, 0, 0 };
+// bool buttonIsPressed[] = { false, false, false, false, false, false };
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 // bool buttonPressed(int button) {
 //   switch (button) {
 //     case yellowButton:
-//       return analogRead(yellowButton) < 250;
+//       return analogRead(yellowButton) < 100;
 //       break;
 
 //     case redButton:
-//       return analogRead(redButton) > 130;
+//       return analogRead(redButton) > 1;
 //       break;
 
 //     case upPin:
@@ -36,20 +36,25 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 //       break;
 
 //     case downPin:
-//       return analogRead(downPin) < 250;
+//       return analogRead(downPin) > 0;
 //       break;
 
 //     case leftPin:
-//       return analogRead(leftPin) < 300;
+//       return analogRead(leftPin) < 100;
 //       break;
 
 //     case rightPin:
-//       return analogRead(rightPin) < 400;
+//       return analogRead(rightPin) < 50;
+//       break;
+
+//     default:
+//       return false;
 //       break;
 //   }
 // }
 
 void setup() {
+  Serial.begin(9600);
   // initialize LCD
   lcd.init();
   // turn on LCD backlight
@@ -65,18 +70,12 @@ void setup() {
 }
 
 void loop() {
-  // for (int currentButton = 0; currentButton < sizeof(buttons); currentButton++) {
+  // for (int currentButton = 0; currentButton < 6; currentButton++) {
+  //   buttonIsPressed[currentButton] = false;  //reset the button
   //   if (buttonPressed(buttons[currentButton])) {
   //     Serial.print("button ");
   //     Serial.println(buttons[currentButton]);
-      // if (buttons[currentButton] == 3) {  // -------- Button 3 controls action A
-      //   // do action A stuff here
-      // } else if (buttons[currentButton] == 4) {  // -------- Button 4 controls action B
-      //   // do action B stuff here
-      // } else if (buttons[currentButton] == 5) {  // -------- Button 5 controls action C
-      //   // do action C stuff here
-      // }
-      // buttonIsPressed[currentButton] = false;  //reset the button
+  //     buttonIsPressed[currentButton] = true;  //reset the button
   //   }
   // }
 
@@ -84,9 +83,9 @@ void loop() {
 
   // show the status of each button
   lcd.print(analogRead(redButton) > 1 ? 1 : 0);
-  lcd.print(analogRead(yellowButton) < 100 ? 1 : 0);
-  lcd.print(analogRead(leftPin) < 100 ? 1 : 0);
-  lcd.print(analogRead(rightPin) < 100 ? 1 : 0);
+  lcd.print(analogRead(yellowButton) < 1 ? 1 : 0);
+  lcd.print(analogRead(leftPin) < 15 ? 1 : 0);
+  lcd.print(analogRead(rightPin) < 15 ? 1 : 0);
   lcd.print(analogRead(upPin) > 0 ? 1 : 0);
   lcd.print(analogRead(downPin) > 0 ? 1 : 0);
 
@@ -94,14 +93,16 @@ void loop() {
 
   // calibration
   lcd.print(analogRead(redButton));
+  lcd.print(" ");
   lcd.print(analogRead(yellowButton));
+  lcd.print(" ");
   lcd.print(analogRead(upPin));
+  lcd.print(" ");
   lcd.print(analogRead(downPin));
+  lcd.print(" ");
   lcd.print(analogRead(leftPin));
+  lcd.print(" ");
   lcd.print(analogRead(rightPin));
-
-  if (analogRead(upPin)) {
-  }
 
   delay(100);
   lcd.clear();
