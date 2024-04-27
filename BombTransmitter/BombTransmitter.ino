e0623fa3f5cd69190041a7ed233cb0be018d0d42
William Kang <william@wkang.ca>
2024-04-26 23:21:48 -0400

    throw together a bunch of garbage!!

--- BombTransmitter/BombTransmitter.ino
+++ BombTransmitter/BombTransmitter.ino
@@ -1,63 +1,109 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
 #include <LiquidCrystal_I2C.h>
+
+const int password = "1234321234";
+
+const String receiverAddress = "http://123.123.123.123";
+const char* ssid = "HWDSB-GUEST";
+const char* pass = "Education";
 
 // unplug D12 to upload code :(
 
-String options = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()-";
+// String options = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()-";
 String typedIn = "";
 int currentColumn = 0;
 
-const int redButton = 33;     // goes high when toggled
-const int yellowButton = 32;  // goes low when pushed
-const int upPin = 2;          // goes high when pushed
-const int downPin = 4;        // goes high when pushed
-const int leftPin = 25;       // goes to low when pushed
-const int rightPin = 26;      // goes low when pushed
+const int redButton = 33;
+const int yellowButton = 32;
+const int upPin = 2;
+const int downPin = 4;
+const int leftPin = 25;
+const int rightPin = 26;
 
-int buttons[] = { redButton, yellowButton, upPin, downPin, leftPin, rightPin };
+const int buttons[] = { redButton, yellowButton, upPin, downPin, leftPin, rightPin };
+const int btnType[] = { 1, 0, 0, 0, 1, 1 };  // 1 goes high when pushed, 0 goes low when pushed
+const int btnThresholds[] = { 1, 1, 15, 15, 0, 0 };
 
-// int buttonState[] = { 0, 0, 0, 0, 0, 0 };
-// int lastButtonState[] = { 0, 0, 0, 0, 0, 0 };
-// bool buttonIsPressed[] = { false, false, false, false, false, false };
+int buttonState[] = { 0, 0, 0, 0, 0, 0 };
+int lastButtonState[] = { 0, 0, 0, 0, 0, 0 };
+bool buttonIsPressed[] = { false, false, false, false, false, false };
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-// bool buttonPressed(int button) {
-//   switch (button) {
-//     case yellowButton:
-//       return analogRead(yellowButton) < 100;
-//       break;
+void pressedAction(int button) {
+  switch (button) {
+    case yellowButton:
+      // check
+      if (typedIn == password) {
+        // send a request to receiver
+        if (WiFi.status() == WL_CONNECTED) {
+          HTTPClient http;
+          String serverPath = receiverAddress + "/on";
 
-//     case redButton:
-//       return analogRead(redButton) > 1;
-//       break;
+          // Your Domain name with URL path or IP address with path
+          http.begin(serverPath.c_str());
 
-//     case upPin:
-//       return analogRead(upPin) > 0;
-//       break;
+          // Send HTTP GET request
+          int httpResponseCode = http.GET();
 
-//     case downPin:
-//       return analogRead(downPin) > 0;
-//       break;
+          if (httpResponseCode > 0) {
+            Serial.print("HTTP Response code: ");
+            Serial.println(httpResponseCode);
+          } else {
+            Serial.print("Error code: ");
+            Serial.println(httpResponseCode);
+          }
+          // Free resources
+          http.end();
+        } else {
+          Serial.println("Failed to send signal: WiFi Disconnected");
+        }
+      } else {
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("Incorrect");
+        lcd.setCursor(0, 1);
+        lcd.print("Password!");
+        delay(1000);
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("Password:");
+        lcd.setCursor(0, 1);
+      }
+      break;
 
-//     case leftPin:
-//       return analogRead(leftPin) < 100;
-//       break;
+    case redButton:
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Password:");
+      lcd.setCursor(0, 1);
+      break;
 
-//     case rightPin:
-//       return analogRead(rightPin) < 50;
-//       break;
+    case upPin:
+      typedIn.append("1");
+      break;
 
-//     default:
-//       return false;
-//       break;
-//   }
-// }
+    case downPin:
+      typedIn.append("2");
+      break;
+
+    case leftPin:
+      typedIn.append("3");
+      break;
+
+    case rightPin:
+      typedIn.append("4");
+      break;
+
+    default:
+      break;
+  }
+}
 
 void setup() {
-  Serial.begin(9600);
-  // initialize LCD
+  Serial.begin(115200);
   lcd.init();
-  // turn on LCD backlight
   lcd.backlight();
   pinMode(yellowButton, INPUT);
   pinMode(redButton, INPUT);
@@ -67,43 +113,54 @@ void setup() {
   pinMode(downPin, INPUT);
   lcd.setCursor(0, 0);
   lcd.print("Password:");
+
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
-  // for (int currentButton = 0; currentButton < 6; currentButton++) {
-  //   buttonIsPressed[currentButton] = false;  //reset the button
-  //   if (buttonPressed(buttons[currentButton])) {
-  //     Serial.print("button ");
-  //     Serial.println(buttons[currentButton]);
-  //     buttonIsPressed[currentButton] = true;  //reset the button
-  //   }
-  // }
+  for (int currentButton = 0; currentButton < 6; currentButton++) {
+    buttonIsPressed[currentButton] = btnType[currentButton] == 0
+                                       ? (analogRead(buttons[currentButton]) < btnThresholds[currentButton])
+                                       : (analogRead(buttons[currentButton]) > btnThresholds[currentButton]);
 
-  lcd.setCursor(0, 0);
+    if (buttonIsPressed[currentButton]) {
+      Serial.print("button ");
+      Serial.println(buttons[currentButton]);
+      pressedAction(buttons[currentButton]);
+    }
+  }
 
-  // show the status of each button
-  lcd.print(analogRead(redButton) > 1 ? 1 : 0);
-  lcd.print(analogRead(yellowButton) < 1 ? 1 : 0);
-  lcd.print(analogRead(leftPin) < 15 ? 1 : 0);
-  lcd.print(analogRead(rightPin) < 15 ? 1 : 0);
-  lcd.print(analogRead(upPin) > 0 ? 1 : 0);
-  lcd.print(analogRead(downPin) > 0 ? 1 : 0);
-
-  lcd.setCursor(0, 1);
-
-  // calibration
-  lcd.print(analogRead(redButton));
-  lcd.print(" ");
-  lcd.print(analogRead(yellowButton));
-  lcd.print(" ");
-  lcd.print(analogRead(upPin));
-  lcd.print(" ");
-  lcd.print(analogRead(downPin));
-  lcd.print(" ");
-  lcd.print(analogRead(leftPin));
-  lcd.print(" ");
-  lcd.print(analogRead(rightPin));
-
-  delay(100);
-  lcd.clear();
+  Serial.print("Red | Activated: ");
+  Serial.print(buttonIsPressed[0] ? "🗸" : "✗");
+  Serial.print(" | Value: ");
+  Serial.print(analogRead(redButton));
+  Serial.print("Yellow | Activated: ");
+  Serial.print(buttonIsPressed[1] ? "🗸" : "✗");
+  Serial.print(" | Value: ");
+  Serial.print(analogRead(yellowButton));
+  Serial.print("Up | Activated: ");
+  Serial.print(buttonIsPressed[2] ? "🗸" : "✗");
+  Serial.print(" | Value: ");
+  Serial.print(analogRead(upPin));
+  Serial.print("Down | Activated: ");
+  Serial.print(buttonIsPressed[3] ? "🗸" : "✗");
+  Serial.print(" | Value: ");
+  Serial.print(analogRead(downPin));
+  Serial.print("Left | Activated: ");
+  Serial.print(buttonIsPressed[4] ? "🗸" : "✗");
+  Serial.print(" | Value: ");
+  Serial.print(analogRead(leftPin));
+  Serial.print("Right | Activated: ");
+  Serial.print(buttonIsPressed[5] ? "🗸" : "✗");
+  Serial.print(" | Value: ");
+  Serial.print(analogRead(rightPin));
+  Serial.println("");
 }
